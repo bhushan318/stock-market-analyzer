2c77e384ca602a13f405b946718fa987dddfa2c5
T Nagabhushanam Reddy <nagareddy318@gmail.com>
2025-05-03 12:15:56 +0300

    Add files via upload

--- StockPrediction.py
+++ StockPrediction.py
@@ -131,7 +131,7 @@ class StockModelComparison:
         """Download stock data and prepare initial DataFrame"""
         print(f"📥 Downloading data for {self.ticker}...")
         
-        self.stock_data = yf.download(self.ticker, start=self.start_date, end=self.end_date, threads=False)
+        self.stock_data = yf.download(self.ticker, start=self.start_date, end=self.end_date)
 
         try:
             if len(self.stock_data)==0:
@@ -139,12 +139,14 @@ class StockModelComparison:
                 if self.ticker.endswith('.NS'):
                     # Try without the .NS extension
                     base_ticker = self.ticker.replace('.NS', '')
-                    self.stock_data = yf.download(f"{base_ticker}.NS", start=self.start_date, end=self.end_date, threads=False)
+                    self.stock_data = yf.download(f"{base_ticker}.NS", start=self.start_date, end=self.end_date)
                     
                     # If still no data, try with BSE extension
                     if len(self.stock_data) == 0:
-                        self.stock_data = yf.download(f"{base_ticker}.BO", start=self.start_date, end=self.end_date, threads=False)
-                    
+                        self.stock_data = yf.download(f"{base_ticker}.BO", start=self.start_date, end=self.end_date)
+                if len(self.stock_data)==0:
+                    raise exec("no data downloaded")
+                    exit
         except Exception:
             raise ValueError(f"No data found for ticker {self.ticker}. Please check the ticker symbol.")
             
